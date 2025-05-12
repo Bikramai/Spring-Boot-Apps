1d14489a2c327ea405537e8ea151042ad97c02d9
bikramia <bikram.rhce@gmail.com>
2025-05-12 18:14:56 -0400

    Constructor Injection

--- src/main/java/com/bikram/store/StoreApplication.java
+++ src/main/java/com/bikram/store/StoreApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StoreApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
+//        SpringApplication.run(StoreApplication.class, args);
+        var orderService = new OrderService(new PayPalPaymentService());
+        orderService.placeOrder();
     }
 
 }
