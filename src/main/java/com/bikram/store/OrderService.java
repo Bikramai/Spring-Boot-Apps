865cbedb52bf5657f3a3b9f8ce92f774cd772ad5
bikramia <bikram.rhce@gmail.com>
2025-05-19 14:11:59 -0400

    Configuring Beans Using Annotations

--- src/main/java/com/bikram/store/OrderService.java
+++ src/main/java/com/bikram/store/OrderService.java
@@ -1,11 +1,19 @@
 package com.bikram.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderService {
     private PaymentService paymentService;
 
-//    public OrderService(PaymentService paymentService) {
-//        this.paymentService = paymentService;
+//    public OrderService(PaymentService paymentService, int x) {
 //    }
+//
+//    @Autowired
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder() {
         paymentService.processPayment(100);
